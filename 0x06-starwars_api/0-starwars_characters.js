f64d0357d9407d98b7278262f2fbaf64f5b2b5b7
QuincyMashava <mashavaquincy@gmail.com>
2022-07-28 15:36:02 -0700

    correction

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,25 +1,20 @@
-#!/usr/bin/node
-const request = require('request');
-const API_URL = 'https://swapi-api.hbtn.io/api';
+#!/usr/bin/python3
+import requests
+import json
 
-if (process.argv.length > 2) {
-    request(`${API_URL}/films/${process.argv[2]}/`, (err, _, body) => {
-	if (err) {
-	    console.log(err);
-	}
-	const charactersURL = JSON.parse(body).characters;
-	const charactersName = charactersURL.map(
-	    url => new Promise((resolve, reject) => {
-		request(url, (promiseErr, __, charactersReqBody) => {
-		    if (promiseErr) {
-			reject(promiseErr);
-		    }
-		    resolve(JSON.parse(charactersReqBody).name);
-		});
-	    }));
+x = 0
+def jprint(obj):
+    '''
+    Create a formatted string of the python Json object
+    '''
+text = json.dumps(obj, sort_keys=True, indent=2)
+print(text)
 
-	Promise.all(charactersName)
-	    .then(names => console.log(names.join('\n')))
-	    .catch(allErr => console.log(allErr));
-    });
+parameters={
+    'count': 7
 }
+
+response = requests.get('https://swapi-api.hbtn.io/api/films/')
+print(response.status_code)
+names = response.json()['results'][0]['characters']
+print(names)
